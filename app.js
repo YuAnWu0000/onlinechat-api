3996ed60393dd00fb5dd492f6c506395c1d93c15
Zachary_Wu <andywu03242000@gmail.com>
2020-05-05 21:59:22 +0800

    以websocket收發訊息

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log(`socket ${socket.id} listening`);
+
+  socket.on('login', function (obj) {                
+    console.log(obj);
+    socket.emit('getMessage', {
+      sender: '系統',
+      content: `你好${obj.username}，歡迎使用線上即時聊天功能~`,
+      time: Date.now(),
+    });
+  });
+
+  socket.on('sendMessage', function (obj) {
+    io.emit('getMessage', {
+      sender: obj.sender,
+      content: obj.content,
+      time: obj.time,
+    });
+  });
+
 });
- 
-app.listen(3000, () => {
-    console.log("Server Started. http://localhost:3000");
+
+server.listen(3000, () => {
+  console.log("Server Started. http://localhost:3000");
 });
